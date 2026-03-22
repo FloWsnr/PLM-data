5b41dd76562eebee60b64f4b43c989a0cd84867a
Florian Wiesner <wiesnerflorian94@gmail.com>
2026-03-22 15:53:41 +0100

    start fresh

--- pde_sim/__init__.py
+++
@@ -1,7 +0,0 @@
-"""PDE Simulation Dataset Generator.
-
-A modular framework for generating 2D PDE simulation trajectories
-using py-pde, designed for training vision-language models.
-"""
-
-__version__ = "0.1.0"
--- pde_sim/boundaries/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Boundary condition handling."""
-
-from .factory import BoundaryConditionFactory, create_grid_with_bc
-
-__all__ = [
-    "BoundaryConditionFactory",
-    "create_grid_with_bc",
-]
